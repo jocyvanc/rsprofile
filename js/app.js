e629d6fcb8fd471e8c3290fccefd628a7b0d55b2
Jocyvan Castro <jocyvancastro@pag.ai>
2019-08-27 19:03:19 -0300

    Improvements on layout and persisting quest data

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 var app = angular.module('rsProfile', []);
 
 app.controller('indexCtrl', function($scope, $http) {
+  if (localStorage.getItem("rsQuests")) {
+    $scope.rsQuests = JSON.parse(localStorage.getItem("rsQuests")).quests;
+  }
+
   $scope.getLinks = function() {
     $scope.rsQuestsUrl = "https://apps.runescape.com/runemetrics/quests?user="+$scope.nickname;
   }
@@ -8,5 +12,16 @@ app.controller('indexCtrl', function($scope, $http) {
   $scope.loadData = function() {
     $scope.rsQuestsUrl = null;
     $scope.rsQuests = JSON.parse($scope.rawData).quests;
+    localStorage.setItem("rsQuests", $scope.rawData);
+  }
+
+  $scope.filter = function(quest) {
+    if (quest.status == "COMPLETED" && $scope.completed_filter) {
+      return true;
+    } else if (quest.userEligible && $scope.eligible_filter) {
+      return true;
+    } else {
+      return false;
+    }
   }
 });
